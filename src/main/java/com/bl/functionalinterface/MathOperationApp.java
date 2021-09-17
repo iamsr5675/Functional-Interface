4e9de234b979338a1d63a918e3b70e2e9f025814
iamsr5675 <saurabhranjan5675@gmail.com>
2021-09-17 18:05:57 +0530

    Math Operation App

---
+++ src/main/java/com/bl/functionalinterface/MathOperationApp.java
@@ -0,0 +1,28 @@
+package com.bl.functionalinterface;
+
+@FunctionalInterface
+interface IMathFunction {
+	int calculate(int a, int b);
+	static void printResult(int a, int b, String function, IMathFunction fobj) {
+		System.out.println("Result of " +function+ " is " + fobj.calculate(a, b));
+	}
+	}
+
+public class MathOperationApp {
+	public static void main(String[] args) {
+		IMathFunction add = (a, b) -> a + b;  //IMathFunction add = Integer::sum;
+		IMathFunction subtract = (a, b) -> a - b;
+		IMathFunction multiply = (a, b) -> a * b;
+		IMathFunction divide = (a, b) -> a / b;
+		
+		System.out.println("Addition: " +add.calculate(5, 2));  //Passing Lambda expression
+		System.out.println("Subtraction: " +subtract.calculate(5, 2));
+		System.out.println("Multiplication: " +multiply.calculate(5, 2));
+		System.out.println("Division: " +divide.calculate(5, 2));
+		
+		IMathFunction.printResult(6, 3, "Addition", add); //Passing Lambda expression as a functional type
+		IMathFunction.printResult(6, 3, "Subtraction", subtract);
+		IMathFunction.printResult(6, 3, "Multiplication", multiply);
+		IMathFunction.printResult(6, 3, "Division", divide);
+	}
+}
